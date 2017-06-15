4c4f3b3ccfc597142f2b696dc4db919c612bf7d8
Joel Sene <joelsene@Joels-iMac.local>
2017-06-15 14:45:27 -0300

    Finalizados

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+/* only uppercase */
 $(function() {
-  $('#txtNome').keydown(function (e) {
+  $("#txtNome").keydown(function(e) {
     if (e.shiftKey || e.ctrlKey || e.altKey) {
       e.preventDefault();
     } else {
       var key = e.keyCode;
-      if (!((key == 8) || (key == 32) || (key == 46) || (key >= 35 && key <= 40) || (key >= 65 && key <= 90))) {
+      if (
+        !(
+          key == 8 ||
+          key == 32 ||
+          key == 46 ||
+          (key >= 35 && key <= 40) ||
+          (key >= 65 && key <= 90)
+        )
+      ) {
         e.preventDefault();
       }
     }
   });
 });
-
