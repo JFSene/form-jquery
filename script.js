95c5f45cbd8cedc8a37b0f2617cc20b7f000aa66
Joel Sene <joelsene@Joels-iMac.local>
2017-06-15 14:42:46 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-$.validator.addMethod(
-  "lowercasesymbols",
-  function(value) {
-    return value.match("^[a-zA-Z]{3,16}$");
-  },
-  "Você deve usar somente letra maiúscula."
-);
-
 $(function() {
-  $(".form-principal").validate();
+  $('#txtNome').keydown(function (e) {
+    if (e.shiftKey || e.ctrlKey || e.altKey) {
+      e.preventDefault();
+    } else {
+      var key = e.keyCode;
+      if (!((key == 8) || (key == 32) || (key == 46) || (key >= 35 && key <= 40) || (key >= 65 && key <= 90))) {
+        e.preventDefault();
+      }
+    }
+  });
 });
+
